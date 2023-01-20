09fad5390c22187566bd25488c5b07fe7794b4ba
Yunilda Justo <105021692+Yunilda09@users.noreply.github.com>
2023-01-19 21:54:40 -0400

    Initial commit

---
+++ app/src/main/java/com/edu/ucne/teprestoapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.edu.ucne.teprestoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.edu.ucne.teprestoapp.ui.theme.TePrestoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TePrestoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TePrestoAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/edu/ucne/teprestoapp/TePrestoApp.kt
@@ -0,0 +1,8 @@
+package com.edu.ucne.teprestoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TePrestoApp: Application() {
+}
